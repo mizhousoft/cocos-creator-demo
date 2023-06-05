04b22d01f942dfb6ad0ad3b2cbf97e1661144e14
jacklee <lsp_lishuiping@qq.com>
2023-06-05 12:03:23 +0800

    优化工程

--- with-protobufjs/assets/script/Main.ts
+++ with-protobufjs/assets/script/Main.ts
@@ -2,13 +2,13 @@ import { _decorator, Component, Node } from 'cc';
 import { HelloWorld } from './proto/proto.js';
 const { ccclass, property } = _decorator;
 
-@ccclass('Loading')
-export class Loading extends Component {
+@ccclass('Main')
+export class Main extends Component {
     start() {
         let helloWorld = new HelloWorld();
         helloWorld.id = 100;
 
-        console.log('Loading...' + JSON.stringify(helloWorld));
+        console.log('Loaded Protobufjs object: ' + JSON.stringify(helloWorld));
     }
 
     update(deltaTime: number) {
